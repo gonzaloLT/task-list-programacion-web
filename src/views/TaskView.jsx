abea276f434cbbd57f783ec7ced32e24b623bd25
Gonzalo Barroso <gonzalobarroso50@gmail.com>
2024-09-12 10:22:48 -0300

    Despliegue vercel

--- src/views/TaskView.jsx
+++ src/views/TaskView.jsx
@@ -5,16 +5,15 @@ import TaskList from "../components/TaskList";
 import { useState } from "react";
 
 function TaskView() {
-  const [tasks, setTasks] = useState([]);
+    const [tasks, setTasks] = useState([]);
 
-  return (
-    <div className="container">
-      <Title title={"Lista de tareas de Gonzalo"}></Title>
-      <Form tasks={tasks} setTasks={setTasks}></Form>
-      <TaskList tasks={tasks} setTasks={setTasks}></TaskList>
-    </div>
-  );
+    return (
+        <div className="container">
+            <Title title={"Lista de tareas de Gonzalo"}></Title>
+            <Form tasks={tasks} setTasks={setTasks}></Form>
+            <TaskList tasks={tasks} setTasks={setTasks}></TaskList>
+        </div>
+    );
 }
-
 
 export default TaskView;
